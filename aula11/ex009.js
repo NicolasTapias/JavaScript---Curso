eaf0246463430e6a6d9d6f0dbdba023b45794ddc
NicolasTapias <nicolastapiasm@gmail.com>
2024-05-31 15:16:24 -0300

    cursoJS aula14

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Brasieiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
---
+++ aula12/ex012.js
@@ -0,0 +1,8 @@
+var idade = 16
+if( idade < 16){
+    console.log('Não vota')
+} else if (idade <19){
+        console.log('voto opcional')
+    } else if (idade >=19){
+        console.log ('voto obrigatorio')
+    }
